7fb5d6b2c0559873ffc01fc19cff93636038f3f6
PersonWomanManCameraTv <161236693+PersonWomanManCameraTv@users.noreply.github.com>
2024-03-11 11:01:39 -0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,6 @@
- 
-//let {ClientInputConfig} = require('./config.js')
-
 const {Helpers} = require('./helpers.js')
 const {DiscordHandler} = require('./discord.js')
 
-//const config = new ClientInputConfig()
  
 const Discord = new DiscordHandler();
 
